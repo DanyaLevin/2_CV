5586b58be776dfca65aa328174a767a599a1f32b
Daniil Levin <daniilevin.work@icloud.com>
2024-04-05 15:01:15 +0200

    20240405

--- script.js
+++ script.js
@@ -5,35 +5,23 @@ let expandAllButton = document.getElementById('expand-all-button');
 let revertButton = document.getElementById('revert-button');
 
 let itemsToExpand = [
-    'exp-5-com-more', 'exp-5-ach-more', 'exp-5-man-more', 
-    'exp-4-com-more', 'exp-4-ach-more', 'exp-4-cat-more', 'exp-4-man-more', 
-    'exp-3-com-more', 'exp-3-ach-more', 'exp-3-cat-more', 'exp-3-man-more', 
-    'exp-2-com-more', 'exp-2-ach-more', 'exp-2-cat-more', 'exp-2-man-more', 
-    'exp-1-com-more', 'exp-1-ach-more', 'exp-1-cat-more', 'exp-1-man-more', 
+    'exp-6-com-more',
+    'exp-5-com-more', 'exp-5-ach-more',                   'exp-5-man-more',
+    'exp-4-com-more', 'exp-4-ach-more', 'exp-4-cat-more', 'exp-4-man-more',
+    'exp-3-com-more', 'exp-3-ach-more', 'exp-3-cat-more', 'exp-3-man-more',
+    'exp-2-com-more', 'exp-2-ach-more', 'exp-2-cat-more', 'exp-2-man-more',
+    'exp-1-com-more', 'exp-1-ach-more', 'exp-1-cat-more', 'exp-1-man-more',
 
-    'edu-3-est-more', 
-    'edu-2-est-more', 
-    'edu-1-est-more', 
+    'edu-3-est-more',
+    'edu-2-est-more',
+    'edu-1-est-more',
 
-    'tra-4-est-more', 'tra-4-des-more', 
-    'tra-3-est-more', 'tra-3-des-more', 
-    'tra-2-est-more', 'tra-2-des-more', 
+    'tra-6-est-more', 'tra-6-des-more',
+    'tra-5-est-more', 'tra-5-des-more',
+    'tra-4-est-more', 'tra-4-des-more',
+    'tra-3-est-more', 'tra-3-des-more',
+    'tra-2-est-more', 'tra-2-des-more',
     'tra-1-est-more', 'tra-1-des-more'];
-
-let itemsToBold = [
-    'exp-5-com', 'exp-5-ach', 'exp-5-man', 
-    'exp-4-com', 'exp-4-ach', 'exp-4-cat', 'exp-4-man', 
-    'exp-3-com', 'exp-3-ach', 'exp-3-cat', 'exp-3-man', 
-    'exp-2-com', 'exp-2-ach', 'exp-2-cat', 'exp-2-man', 
-    'exp-1-com', 'exp-1-ach', 'exp-1-cat', 'exp-1-man', 
-    'edu-3-est', 'edu-2-est', 'edu-1-est', 'tra-4-est',
-
-    'tra-4-des', 
-    'tra-3-est', 'tra-3-des', 
-    'tra-2-est', 'tra-2-des', 
-    'tra-1-est', 'tra-1-des'];
-
-
 
 function expandAllInfo() {
     for (i=0; i<itemsToExpand.length; i++) {
@@ -79,6 +67,8 @@ function nextPhoto () {
 
 // The following cunt allows expanding fields one by one
 
+toggleMore('exp-6-com', 'exp-6-com-more');
+
 toggleMore('exp-5-com', 'exp-5-com-more');
 toggleMore('exp-5-ach', 'exp-5-ach-more');
 toggleMore('exp-5-man', 'exp-5-man-more');
@@ -107,6 +97,10 @@ toggleMore('edu-3-est', 'edu-3-est-more');
 toggleMore('edu-2-est', 'edu-2-est-more');
 toggleMore('edu-1-est', 'edu-1-est-more');
 
+toggleMore('tra-6-est', 'tra-6-est-more');
+toggleMore('tra-6-des', 'tra-6-des-more');
+toggleMore('tra-5-est', 'tra-5-est-more');
+toggleMore('tra-5-des', 'tra-5-des-more');
 toggleMore('tra-4-est', 'tra-4-est-more');
 toggleMore('tra-4-des', 'tra-4-des-more');
 toggleMore('tra-3-est', 'tra-3-est-more');
